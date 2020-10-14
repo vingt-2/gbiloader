0f0d12e0cc094316e842e21273866245fa134f46
vingt-2 <vingt-2@hotmail.fr>
2020-10-13 20:58:35 -0400

    Ported gbiloader to using sd2sp2

--- source/main.c
+++ source/main.c
@@ -108,74 +108,16 @@ void Initialise()
 
 static int initFAT()
 {
-	int slotagecko = 0;
-	int i = 0;
-	s32 ret, memsize, sectsize;
+	__io_gcsd2.startup();
 
-	for (i = 0; i < 10; i++)
+	if (!__io_gcsd2.isInserted())
 	{
-		ret = CARD_ProbeEx(CARD_SLOTA, &memsize, &sectsize);
-
-		if (ret == CARD_ERROR_WRONGDEVICE)
-		{
-			slotagecko = 1;
-			break;
-		}
+		return 0;
 	}
 
-	if (slotagecko)
+	if (!fatMountSimple("fat", &__io_gcsd2))
 	{
-		// Memcard in SLOT B, SD gecko in SLOT A
-
-		// This will ensure SD gecko is recognized if inserted or changed to another slot after GCMM is executed
-		for (i = 0; i < 10; i++)
-		{
-			ret = CARD_Probe(CARD_SLOTA);
-
-			if (ret == CARD_ERROR_WRONGDEVICE)
-			{
-				break;
-			}
-		}
-
-		__io_gcsda.startup();
-
-		if (!__io_gcsda.isInserted())
-		{
-			return 0;
-		}
-
-		if (!fatMountSimple("fat", &__io_gcsda))
-		{
-			return 0;
-		}
-	}
-	else
-	{
-		//Memcard in SLOT A, SD gecko in SLOT B
-
-		//This will ensure SD gecko is recognized if inserted or changed to another slot after GCMM is executed
-		for (i = 0; i < 10; i++)
-		{
-			ret = CARD_Probe(CARD_SLOTB);
-
-			if (ret == CARD_ERROR_WRONGDEVICE)
-			{
-				break;
-			}
-		}
-
-		__io_gcsdb.startup();
-
-		if (!__io_gcsdb.isInserted())
-		{
-			return 0;
-		}
-
-		if (!fatMountSimple("fat", &__io_gcsdb))
-		{
-			return 0;
-		}
+		return 0;
 	}
 
 	return 1;
